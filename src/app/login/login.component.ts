f630ce2afff9ee4fc327d7e66207a87a4204adf0
rcbull <rcbull@gmail.com>
2019-02-16 20:00:26 -0200

    validando email nos forms de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,16 +9,52 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   formEstabelecimento: FormGroup;
-  formLogin: FormGroup;
+  formTitular: FormGroup;
+  submitted = false;
+  success = false;
+
+  estabelecimentoInvalid = false;
+  titularInvalid = false;
 
   constructor(private formBuilder: FormBuilder) {
   }
 
   ngOnInit() {
+    this.formEstabelecimento = this.formBuilder.group(
+      {email: ['', [Validators.required, Validators.email]]}
+    );
+    this.formTitular = this.formBuilder.group(
+      {email: ['', [Validators.required, Validators.email]]}
+    );
+  }
 
-    this.formEstabelecimento = this.formBuilder.group({email: ['', Validators.required]});
-    this.formLogin = this.formBuilder.group({email: ['', Validators.required]});
+  saveTitular() {
+    this.submitted = true;
 
+    console.log(this.formTitular.value.email);
+
+    if (this.formTitular.invalid) {
+      this.titularInvalid = true;
+    } else {
+      this.titularInvalid = false;
+    }
+
+    this.success = true;
+  }
+
+  saveEstabelecimento() {
+    this.submitted = true;
+
+    console.log(this.formEstabelecimento.value.email);
+
+    console.log(this.formEstabelecimento.invalid)
+    if (this.formEstabelecimento.invalid) {
+      this.estabelecimentoInvalid = true;
+    } else {
+      this.estabelecimentoInvalid = false;
+    }
+
+    this.success = true;
   }
 
 }
